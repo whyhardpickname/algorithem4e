9645fddce7fb365d05d738516f996357fad29cf8
whyhardpickname <13zxchen3@gmail.com>
2020-08-07 13:27:45 +0800

    Ex1_1_1, Ex1_1_2

---
+++ ch01/Ex1_1_1.java
@@ -0,0 +1,12 @@
+public class Ex1_1_1
+{
+	public static void main(String[] args)
+	{
+		int a = 7;
+		double b = 200.0000002;
+		boolean c = true;
+		System.out.println(a == (0 + 15) / 2);
+		System.out.println(2.0e-6 * 100000000.1);
+		System.out.println(c == (true && false || true && true));
+	}
+}
---
+++ ch01/Ex1_1_2.java
@@ -0,0 +1,13 @@
+public class Ex1_1_1
+{
+	public static void main(String[] args)
+	{
+		double a = ;
+		double b = 200.0000002;
+		boolean c = true;
+		
+		System.out.println(a == (0 + 15) / 2);
+		System.out.println(2.0e-6 * 100000000.1);
+		System.out.println(c == (true && false || true && true));
+	}
+}
