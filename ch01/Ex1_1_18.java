cc6cc1fcf294322b21885c87624ce42938416232
whyhardpickname <13zxchen3@gmail.com>
2020-08-19 13:26:07 +0800

    Ex1_1_18

--- ch01/Ex1_1_18.java
+++ ch01/Ex1_1_18.java
@@ -8,18 +8,48 @@ public class Ex1_1_18
 	public static void main(String[] args)
 	{
 		/*
-			stack over flow error
+			50
+			33
+			
 		*/
-		System.out.println(exR1(6));
+		System.out.println("mystery(2, 25) = " + 
+			mystery(2, 25));
+		System.out.println("mystery(3, 11) = " + 
+			mystery(3, 11));
+		
+		System.out.println("mystery2(2, 8) = " + 
+			mystery2(2, 8));
+		System.out.println("mystery2(2, 10) = " + 
+			mystery2(2, 10));
+		
+			
 	}
 	
-	public static String exR1(int n)
+	public static int mystery(int a, int b)
 	{
-		String s = exR1(n - 3) + n + exR1(n - 2) + n;
-		if (n <= 0) 
+		if (b == 0)
 		{
-			return "";
+			return 0;
 		}
-		return s;
+		
+		if (b % 2 == 0)
+		{
+			return mystery(a + a, b / 2);
+		}
+		return mystery(a + a, b / 2) + a;
+	}
+	
+	public static int mystery2(int a, int b)
+	{
+		if (b == 0)
+		{
+			return 1;
+		}
+		
+		if (b % 2 == 0)
+		{
+			return mystery(a * a, b / 2);
+		}
+		return mystery(a * a, b / 2) * a;
 	}
 }
