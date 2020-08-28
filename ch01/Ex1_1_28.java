ccaee05e724e3e9c1eea2cce0a8b75a2a7919fd0
whyhardpickname <13zxchen3@gmail.com>
2020-08-28 14:07:45 +0800

    deduplicate array

--- ch01/Ex1_1_28.java
+++ ch01/Ex1_1_28.java
@@ -10,16 +10,54 @@ import java.util.ArrayList;
 
 public class Ex1_1_28
 {
-	public static int dedupliacate(int[] sorted)
+	public static int[] dedupliacate(int[] sorted)
 	{
-		
+		int i = 0;
+		int j = 0;
+		if (sorted == null)
+		{
+			return null;
+		}
+		if (sorted.length == 0)
+		{
+			return new int[] {};
+		}
+		while (j < sorted.length)
+		{
+			while (j < sorted.length && sorted[i] == sorted[j])
+			{
+				j++;
+			}
+			if (j < sorted.length)
+			{
+				sorted[++i] = sorted[j];
+			}
+		}
+		return getArray(sorted, i + 1);
+	}
+	
+	public static int[] getArray(int[] array, int to)
+	{
+		if (array == null)
+		{
+			return null;
+		}
+		int[] copied = new int[to];
+		for (int i = 0; i < to; i++)
+		{
+			copied[i] = array[i];
+		}
+		return copied;
 	}
 	
 	public static void main(String[] args)
 	{
-		In in = new In(args[1]);
+		In in = new In(args[0]);
 		int[] whiteList = in.readAllInts();
 		Arrays.sort(whiteList);
-	
+		for (int i : dedupliacate(whiteList))
+		{
+			System.out.print( i + " ");
+		}
 	}
 }
