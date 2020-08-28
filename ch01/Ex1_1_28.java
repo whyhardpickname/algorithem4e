7e1cd8dc9b38ac4d387032b8ded87bc68050f210
whyhardpickname <13zxchen3@gmail.com>
2020-08-28 13:27:56 +0800

    Ex1_1_28.java

---
+++ ch01/Ex1_1_28.java
@@ -0,0 +1,25 @@
+//javac -cp C:\Users\MarkChern\algs4\algs4.jar
+//javac -cp C:\Users\Administrator\algs4\algs4.jar
+//java -cp C:\Users\MarkChern\algs4\algs4.jar;
+//java -cp C:\Users\Administrator\algs4\algs4.jar;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Ex1_1_28
+{
+	public static int dedupliacate(int[] sorted)
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		In in = new In(args[1]);
+		int[] whiteList = in.readAllInts();
+		Arrays.sort(whiteList);
+	
+	}
+}
