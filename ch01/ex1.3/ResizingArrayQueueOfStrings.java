0d791f26b01ab8205d58f741598b6f59e221ed39
whyhardpickname <13zxchen3@gmail.com>
2020-11-07 17:17:18 +0800

    ResizingArrayQueueOfStrings

--- ch01/ex1.3/ResizingArrayQueueOfStrings.java
+++ ch01/ex1.3/ResizingArrayQueueOfStrings.java
@@ -1,47 +1,97 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import javax.sound.midi.Soundbank;
+
+/**
+ * @author MarkChern
+ * date 2020/11/7 16:17
+ */
 public class ResizingArrayQueueOfStrings
 {
-    private String[] strings;
-    private int n;
-    private int first;
-    private int last;
+    private int n = 0;
+    private String[] strings = new String[1];
+    private int first = 0;
+    private int last = 0;
 
-    public ResizingArrayQueueOfStrings()
+    public ResizingArrayQueueOfStrings(){}
+
+    public boolean isEmpty()
     {
-        strings = new String[2];
-        n = 0;
+        return n == 0;
     }
 
-    private void lengthen(int max)
+    public int size()
     {
-        String[] temp = new String[max];
-        for (int i= 0; i < strings.length; i++)
-        {
-            temp[i] = strings[i];
-        }
-        strings = temp;
-    }
-
-    private void shorten(int min)
-    {
-        String[] temp = new String[min];
-        for (int i= 0; i < last; i++)
-        {
-            temp[i] = strings[i + first];
-        }
-        strings = temp;
+        return n;
     }
 
     public void enqueue(String s)
     {
-        if (n == strings.length)
+        if (last == strings.length)
         {
-            resize(2 * strings.length);
+            if (n < s.length() / 2)
+            {
+                inPlaceRearrange();
+            }
+            else
+            {
+                resize(strings.length * 2);
+            }
         }
-        strings[n++] = s;
+        strings[last++] = s;
+        n++;
     }
 
     public String dequeue()
     {
-        String temp = strings[]
+        if (n == strings.length / 4)
+        {
+            resize(strings.length / 2);
+        }
+        String temp = strings[first];
+        strings[first++] = null;
+        n--;
+        return temp;
+    }
+
+    private void resize(int len)
+    {
+        String[] temp = new String[len];
+        for (int i = first; i < last; i++)
+        {
+            temp[i - first] = strings[i];
+        }
+        strings = temp;
+        last -= first;
+        first = 0;
+    }
+    private void inPlaceRearrange()
+    {
+        for (int i = first; i < last; i++)
+        {
+            strings[i - first] = strings[i];
+        }
+        last -= first;
+        first = 0;
+
+    }
+
+    public static void main(String[] args)
+    {
+        ResizingArrayQueueOfStrings s;
+        s = new ResizingArrayQueueOfStrings();
+        while (!StdIn.isEmpty())
+        {
+            System.out.println(s.n * 1.0 / s.strings.length * 100.0 + "%");
+            String item = StdIn.readString();
+            if (!item.equals("-"))
+            {
+                s.enqueue(item);
+            } else if (!s.isEmpty())
+            {
+                StdOut.print(s.dequeue() + " ");
+            }
+        }
     }
 }
